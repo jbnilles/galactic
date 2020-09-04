9540bbd7f5d26b3c2876c3cd939c28778e1153e8
jbnilles <jbnilles24@gmail.com>
2020-09-04 10:42:22 -0700

    add test for mercury calculation

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,15 @@
+import Calculator from '../src/calculator.js'
+
+describe('Calculator', () => {
+  let calc;
+  beforeEach(() => {
+    calc = new Calculator(100);
+    
+  });
+  test('should correctly calculate years on mercury given earth years', () => {
+    expect(calc.calcMercury()).toEqual(4170);
+  }); 
+  
+
+  
+});
---
+++ src/calculator.js
@@ -0,0 +1,8 @@
+export default class Calculator {
+  constructor(earthYears){
+  this.earthYears = earthYears;
+  
+  }
+
+  
+}
