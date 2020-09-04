fefccd985e56b248c4db5ca79904323025688f20
jbnilles <jbnilles24@gmail.com>
2020-09-04 10:38:08 -0700

    add initial files and folders

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './js/triangle.js';
+import Rectangle from './js/rectangle.js';
+
+$(document).ready(function() {
+  
+});
