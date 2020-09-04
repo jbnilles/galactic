79685332048e97bcdd272f52cf864801af631388
jbnilles <jbnilles24@gmail.com>
2020-09-04 10:43:08 -0700

    add js code for mercury test

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,9 @@
 export default class Calculator {
   constructor(earthYears){
   this.earthYears = earthYears;
-  
   }
-
+  calcMercury() {
+    return Math.round(this.earthYears / .24);
+  };
   
 }
